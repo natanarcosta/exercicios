39fc4d0e4af895f6b42023e849c78bd0369902b2
Natan A. Costa <natanarcosta@gmail.com>
2021-03-14 13:02:45 -0300

    Primeira resolução do exercicio de Exceções. Resultados conforme o esperado

---
+++ Exercicio-146/Exercicio-146/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercicio_146.Entities.Exceptions;
+
+namespace Exercicio_146.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (Balance == 0)
+            {
+                throw new DomainException("Saldo deve ser positivo!");
+            }
+            if ( amount > WithdrawLimit)
+            {
+                throw new DomainException("Saque deve ser menor que o limite de saques!");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ Exercicio-146/Exercicio-146/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_146.Entities.Exceptions;
+using Exercicio_146.Entities;
+
+namespace Exercicio_146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Dados da conta:");
+                Console.Write("Número: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string titular = Console.ReadLine();
+                Console.Write("Depósito Inicial: $");
+                double inicial = double.Parse(Console.ReadLine());
+                Console.Write("Limite de saque: $");
+                double limiteDeSaque = double.Parse(Console.ReadLine());
+
+                Account conta = new Account(numero, titular, inicial, limiteDeSaque);
+
+                Console.Write("Valor do saque: ");
+                double saque = double.Parse(Console.ReadLine());
+                conta.Withdraw(saque);
+                Console.Write("Novo saldo: " + conta.Balance);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro!" + e.Message);
+            }
+        }
+    }
+}
