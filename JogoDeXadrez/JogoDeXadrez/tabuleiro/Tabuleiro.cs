118d71dc36d6dc519d1dc34e058d74051c488563
Natan A. Costa <natanarcosta@gmail.com>
2021-03-14 15:20:44 -0300

    Tratando erros com Exceção pesonalizada

--- JogoDeXadrez/JogoDeXadrez/tabuleiro/Tabuleiro.cs
+++ JogoDeXadrez/JogoDeXadrez/tabuleiro/Tabuleiro.cs
@@ -14,16 +14,45 @@ namespace tabuleiro
         {
             Linhas = linhas;
             Colunas = colunas;
-            Pecas = new Peca[linhas,colunas];
+            Pecas = new Peca[linhas, colunas];
         }
         public Peca peca(int linha, int coluna)
         {
             //Retorna o que há nessa posição da Matriz ( peça ou vazio)
-            return Pecas[linha, coluna]; 
+            return Pecas[linha, coluna];
+        }
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
         }
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
