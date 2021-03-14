30f558d13b09a9782cac73d75c9252e38ed4ef06
Natan A. Costa <natanarcosta@gmail.com>
2021-03-14 14:21:15 -0300

    Criada a classe Posição

---
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using tabuleiro;
+
+namespace JogoDeXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
