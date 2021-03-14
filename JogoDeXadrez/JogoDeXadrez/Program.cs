fbfa5bc9379e2df44986fa4f21a1de662af52e9c
Natan A. Costa <natanarcosta@gmail.com>
2021-03-14 15:43:58 -0300

    Melhorando a impressão do tabuleiro (cores etc)

--- JogoDeXadrez/JogoDeXadrez/Program.cs
+++ JogoDeXadrez/JogoDeXadrez/Program.cs
@@ -8,10 +8,17 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+            tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+            Tela.ImprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
--- JogoDeXadrez/JogoDeXadrez/Tela.cs
+++ JogoDeXadrez/JogoDeXadrez/Tela.cs
@@ -11,6 +11,8 @@ namespace JogoDeXadrez
         {
             for (int i = 0; i<tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j< tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -19,10 +21,25 @@ namespace JogoDeXadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        ImprimirPeca(tab.peca(i, j));
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca + " ");
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca +  " ");
+                Console.ForegroundColor = aux;
             }
         }
     }
