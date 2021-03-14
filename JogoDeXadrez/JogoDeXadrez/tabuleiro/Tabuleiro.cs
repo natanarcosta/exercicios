840b58ecbc04d786e79738f108e6d14ab8834816
Natan A. Costa <natanarcosta@gmail.com>
2021-03-14 15:03:20 -0300

    Começando a colocar peças no  tabuleiro.

--- JogoDeXadrez/JogoDeXadrez/tabuleiro/Tabuleiro.cs
+++ JogoDeXadrez/JogoDeXadrez/tabuleiro/Tabuleiro.cs
@@ -8,18 +8,22 @@ namespace tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        private Peca[,] pecas;
+        private Peca[,] Pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
             Linhas = linhas;
             Colunas = colunas;
-            pecas = new Peca[linhas,colunas];
+            Pecas = new Peca[linhas,colunas];
         }
         public Peca peca(int linha, int coluna)
         {
             //Retorna o que há nessa posição da Matriz ( peça ou vazio)
-            return pecas[linha, coluna]; 
+            return Pecas[linha, coluna]; 
+        }
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
         }
     }
 }
