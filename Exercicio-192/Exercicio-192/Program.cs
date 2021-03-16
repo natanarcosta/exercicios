f7e2f562f7c0ec1a91a443dfe5de4e977f343c86
Natan A. Costa <natanarcosta@gmail.com>
2021-03-15 21:06:08 -0300

    Exercício resolvido e resultados foram conforme o esperado.

---
+++ Exercicio-192/Exercicio-192/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio_192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\natan\Desktop\C#\Exercicios\Exercicio-192\source\itens.csv";
+            string targetPath = @"C:\Users\natan\Desktop\C#\Exercicios\Exercicio-192\source\out\summary.csv";
+
+            try
+            {
+                using (StreamReader reader = File.OpenText(sourcePath))
+                {
+                    using (StreamWriter writer = File.AppendText(targetPath))
+                    {
+
+                        string[] csvLines = File.ReadAllLines(sourcePath);
+
+
+
+                        for (int i = 0; i < csvLines.Length; i++)
+                        {
+                            string itemName = csvLines[i].Split(',')[0];
+                            double itemPrice = double.Parse(csvLines[i].Split(',')[1], CultureInfo.InvariantCulture);
+                            double quantity = int.Parse(csvLines[i].Split(',')[2], CultureInfo.InvariantCulture);
+                            double total = itemPrice * quantity;
+                            string summary = itemName + "," + total.ToString("F2", CultureInfo.InvariantCulture);
+                            writer.WriteLine(summary);
+                            Console.WriteLine("Escrito " + summary);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
