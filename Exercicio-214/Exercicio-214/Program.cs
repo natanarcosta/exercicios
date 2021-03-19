7154dcc2e9a6c075aaedafa5a6b0b7cff7158951
Natan A. Costa <natanarcosta@gmail.com>
2021-03-18 23:01:06 -0300

    Exercício resolvido com resultados iguais aos esperados.

---
+++ Exercicio-214/Exercicio-214/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercicio_214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+            Dictionary<string, int> apuracao = new Dictionary<string, int>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] candidato = sr.ReadLine().Split(',');
+                    string nome = candidato[0];
+                    int votos = int.Parse(candidato[1]);
+                    if (apuracao.ContainsKey(nome))
+                    {
+                        int temp = apuracao[nome];
+                        apuracao.Remove(nome);
+                        apuracao.Add(nome, temp + votos);
+                    }
+                    else
+                    {
+                        apuracao.Add(nome, votos);
+                    }
+                }
+                foreach (var x in apuracao)
+                {
+                    Console.WriteLine(x);
+                }
+
+            }
+        }
+    }
+}
