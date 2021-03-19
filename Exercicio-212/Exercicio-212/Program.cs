a1769bd01fd79d205436b0dddab1155a1b0b1cca
Natan A. Costa <natanarcosta@gmail.com>
2021-03-18 22:25:25 -0300

    Exercício resolvido com resultados iguais aos esperados. Código do professor ficou diferente.

---
+++ Exercicio-212/Exercicio-212/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students for course A? ");
+            int a = int.Parse(Console.ReadLine());
+            HashSet<int> students = new HashSet<int>();
+            AddStudents(a, students);
+            Console.Write("How many students for course B? ");
+            int b = int.Parse(Console.ReadLine());
+            AddStudents(b, students);
+            Console.Write("How many students for course C? ");
+            int c = int.Parse(Console.ReadLine());
+            AddStudents(c, students);
+
+            Console.WriteLine("Total students: " + students.Count);
+        }
+        static void AddStudents(int quantity, HashSet<int> set)
+        {
+            for(int i = 0; i < quantity; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                set.Add(id);
+            }
+        }
+    }
+}
